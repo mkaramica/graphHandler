7fe0ac8c54ff7b7b6f66fff57bcb920e79703222
mkaramica <53634505+mkaramica@users.noreply.github.com>
2023-03-09 14:48:14 -0500

    Add my info and contact.

--- src/components/performImage.jsx
+++ src/components/performImage.jsx
@@ -110,6 +110,13 @@ const PerformImage = () => {
           )}
         </div>
       )}
+      <div className="info-section">
+        <h2>About Me</h2>
+        <p>Name: Mahdi Karami</p>
+        <p>Email: mahdi.karami@gmail.com</p>
+        <p>Cellphone: (+1) 226-344-7809</p>
+        <p>LinkedIn: <a href="https://www.linkedin.com/in/mahdikarami">https://www.linkedin.com/in/mahdikarami</a></p>
+      </div>
     </div>
   );
 };
